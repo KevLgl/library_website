6256662aec1b7b3ca6b06efbeeaffbb09a0277ef
Kevin Laguel <laguel.kev.pro@gmail.com>
2020-10-27 21:44:50 +0100

    add user selected on user.php

--- view/userView.php
+++ view/userView.php
@@ -1,13 +1,28 @@
 <?php
-
 include 'template/header.php';
-
-
-var_dump($user);// FAIRE LA VUE 
 ?>
 
 <main>
-    <p>l'utilisateur sur lequel on a cliqué s'affiche sur cette page</p>
+    <div class="card w-50 m-3">
+        <div class="card-header text-center">
+        <h2> <?php echo $user->getFirstname() . " " . $user->getLastname() ; ?> </h2>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Age: </h5>
+            <p class="card-text"><?php echo $user->getAge(); ?></p>
+            <hr class="my-4">
+            <h5 class="card-title">City: </h5>
+            <p class="card-text"><?php echo $user->getCity(); ?></p>
+            <hr class="my-4">
+            <h5 class="card-title">Phone: </h5>
+            <p class="card-text"><?php echo $user->getPhone(); ?></p>
+            <hr class="my-4">
+            <h5 class="card-title">Email: </h5>
+            <p class="card-text"><?php echo $user->getEmail(); ?></p>
+            <hr class="my-4">
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
 </main>
 
 
